4a29465d36b3bf8c9ed1c7d3fa4bde9ec791e224
Bcccch <habach05032003@gmail.com>
2024-01-24 16:41:23 +0700

    fix nav bar

--- php files/homepage.php
+++ php files/homepage.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="../styles/homepage.css">
     <link rel="stylesheet" href="../styles/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="../script/slide.js"></script>
 </head>
 
