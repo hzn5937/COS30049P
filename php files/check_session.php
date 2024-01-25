4ee7d8a8a879f9e45224aa0242a9a33872de46a2
Duy <haruna8922@gmail.com>
2024-01-25 19:15:30 +0700

    xoa file thua

--- php files/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    header('Content-Type: application/json');
-
-    $response = array('isLoggedIn' => isset($_SESSION['user_id']));
-    echo json_encode($response);
-?>
