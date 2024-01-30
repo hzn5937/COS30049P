66cc9b3754768f8bc9b0335aa9bdb63ed3570e8c
Bcccch <habach05032003@gmail.com>
2024-01-30 21:39:52 +0700

    Change icon on web browser on all website

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="author" content="Group 02">
     <meta name="description" content="Assignment 01 - Static Website">
     <title>Home Page</title>
+    <link rel="icon" href="./images/weblogo.png">
     <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
