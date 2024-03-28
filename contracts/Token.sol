6c13aafbeec4a037468e4c3e584e353e876177f9
TysonNguyen619 <150374817+TysonNguyen619@users.noreply.github.com>
2024-03-28 20:47:55 +0700

    .sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,13 +1,31 @@
 pragma solidity ^0.4.2;
 
 contract Token {
+    uint public tokenGained;
     uint public tokenAmount;
+    address public ownerAdd;
+    uint public transacId;
 
     function Token() public {
-        tokenAmount = 0;
+        tokenAmount = 0;    
     }
-
+    
     function addToken (uint _amount) public {
         tokenAmount += _amount;
+        setAddress();
+        setId();
     }
+
+    function tokenExchanged (uint _amount) public {
+        tokenGained = _amount;
+    }
+
+    function setAddress() private {
+        ownerAdd = msg.sender; 
+    }
+
+    function setId() private {
+        transacId += 1;
+    }
+
 }
